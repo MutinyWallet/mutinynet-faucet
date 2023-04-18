8ec7eb59535dd82c054346aef361cdfcb3a1f263
Paul Miller <paul@paul.lol>
2023-04-18 12:10:14 -0500

    enforce withdrawal limits

--- src/core.ts
+++ src/core.ts
@@ -11,7 +11,20 @@ export async function sendToAddress(address: string, amountSats: number) {
     const client = new Client({ network: NETWORK, username: USER, password: PASS, host: HOST, port: PORT });
 
     // Dangerously divide by 100_000_000 to convert sats to btc
-    const txid = await client.sendToAddress(address, amountSats / 100_000_000) as string;
+    let howMuch = amountSats / 100000000
+    console.log(`Request to send ${howMuch} BTC to ${address}`)
+
+    // Prevent sending more than 1 BTC
+    howMuch = Math.min(howMuch, 1.0)
+
+    // Prevent sending more than half the current balance
+    const currentBalance = await client.getBalance('*', 110)
+    console.log(`Current node balance: ${currentBalance}`);
+    howMuch = Math.min(howMuch, currentBalance / 2);
+
+    console.log(`Actually sending ${howMuch} BTC to ${address}`)
+
+    const txid = await client.sendToAddress(address, howMuch) as string;
 
     return txid
 }
